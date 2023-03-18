fafa7f175e3396f1f7b78905bdbe35cd621813fa
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-03-18 09:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+  var inputData = document.querySelector("#rope-lengths").value;
+  var inputArr=inputData.split(",");
+	for(var i=0;i<inputArr.length;i++){
+		inputArr[i]= Number(inputArr[i]);
+	}
+	
+	 inputArr.sort(function(a,b){
+	  return a-b;
+  });
+	var cost = 0;
+	while (inputArr.length>1) {
+		var priceRope = inputArr[0]+ inputArr[1];
+		cost += priceRope;
+		inputArr.splice(0,2);
+		inputArr.push(priceRope);
+		inputArr.sort(function (a,b) {
+			return a-b;
+		});
+	}
+	return cost;
+	
 }  
